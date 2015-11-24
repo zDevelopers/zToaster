a5227e82054c0cdc4ce5678e1fe5931b9b64c767
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2015-11-24 20:44:30 +0100

    I made a toaster.

---
+++ src/main/java/fr/zcraft/ztoaster/ToastExplorer.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright or © or Copr. ZLib contributors (2015)
+ *
+ * This software is governed by the CeCILL-B license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-B
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
+
+package fr.zcraft.ztoaster;
+
+import fr.zcraft.zlib.components.gui.ExplorerGui;
+import fr.zcraft.zlib.components.gui.GuiUtils;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class ToastExplorer extends ExplorerGui<Toast>
+{
+    @Override
+    protected void onUpdate()
+    {
+        setTitle("Toaster contents");
+        
+        setData(Toaster.getToasts());
+        
+        //DO NOT TOUCH THE TOASTS !
+        setMode(ExplorerGui.Mode.READONLY);
+        
+    }
+    
+    @Override
+    protected ItemStack getViewItem(Toast toast)
+    {
+        if(toast.getStatus().equals(Toast.CookingStatus.COOKED))
+            return GuiUtils.makeItem(Material.GRILLED_PORK, "Cooked toast #" + toast.getToastId());
+        else
+            return GuiUtils.makeItem(Material.PORK, "Raw toast #" + toast.getToastId());
+    }
+}
---
+++ src/main/java/fr/zcraft/ztoaster/Toaster.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright or © or Copr. ZLib contributors (2015)
+ *
+ * This software is governed by the CeCILL-B license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-B
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
+
+package fr.zcraft.ztoaster;
+
+import fr.zcraft.zlib.components.commands.Commands;
+import fr.zcraft.zlib.components.gui.Gui;
+import fr.zcraft.zlib.core.ZPlugin;
+import fr.zcraft.zlib.tools.PluginLogger;
+import fr.zcraft.ztoaster.commands.AddCommand;
+import fr.zcraft.ztoaster.commands.OpenCommand;
+import java.util.ArrayList;
+
+public class Toaster extends ZPlugin 
+{
+    /**
+     * A counter for all the toasts created (until toaster restart).
+     */
+    static private int toastCounter = 0;
+    
+    /**
+     * A list of all the toasts.
+     */
+    static private ArrayList<Toast> toasts;
+    
+    @Override
+    public void onEnable()
+    {
+        PluginLogger.info("Setting up toaster.");
+        
+        toasts = new ArrayList<>();
+        toastCounter = 0;
+        
+        loadComponents(Gui.class, Commands.class, ToasterWorker.class);
+        
+        Commands.register("toaster", AddCommand.class, OpenCommand.class);
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        PluginLogger.info("Unplugging toaster.");
+    }
+    
+    /**
+     * @return The id for a new toast.
+     */
+    static public int newToastId()
+    {
+        return toastCounter++;
+    }
+    
+    /**
+     * @return an array of all the toasts ever created (until toaster restart).
+     */
+    static public Toast[] getToasts()
+    {
+        return toasts.toArray(new Toast[toasts.size()]);
+    }
+    
+    /**
+     * Adds a new toast to the world.
+     * @return the newly created toast.
+     */
+    static public Toast newToast()
+    {
+        Toast toast = new Toast();
+        toasts.add(toast);
+        return toast;
+    }
+    
+}
